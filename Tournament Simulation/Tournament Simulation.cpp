2c5491383df8638311398d70d641ddec69eecf6b
Kian2002 <canadakurdistan@gmail.com>
2023-04-22 01:53:19 -0700

    create groups, seed best teams, start simulation

--- Tournament Simulation/Tournament Simulation.cpp
+++ Tournament Simulation/Tournament Simulation.cpp
@@ -1,15 +1,48 @@
 // Tournament Simulation.cpp : This file contains the 'main' function.
 
-#include <iostream>
-
 #include "Group.h"
 #include "TeamData.h"
 
 int main()
 {
-	std::sort(teams.begin(), teams.end(),
-	          [](const Team& a, const Team& b) { return a.record.rating > b.record.rating; });
+	std::vector<Team> seededTeams;
 
-	Group group(teams);
-	group.playGroupMatch();
+	for (int i = 0; i < 8; i++)
+	{
+		seededTeams.push_back(teams[i]);
+	}
+
+	teams.erase(teams.begin(), teams.begin() + 8);
+	teams.shrink_to_fit();
+
+	std::vector<Group> groups;
+	int groupIndex = 0;
+
+	for (int i = 0; i < 8; i++)
+	{
+		Group group;
+		group.teams.push_back(seededTeams[groupIndex]);
+		groups.push_back(group);
+		groupIndex++;
+	}
+
+	constexpr int maxRandomOffset = 10;
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> dist(-maxRandomOffset, maxRandomOffset);
+	std::shuffle(teams.begin(), teams.end(), rng);
+
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			groups[i].teams.push_back(teams[i * 3 + j]);
+		}
+	}
+
+
+	for (int i = 0; i < 8; i++)
+	{
+		groups[i].playGroupMatch();
+	}
 }
