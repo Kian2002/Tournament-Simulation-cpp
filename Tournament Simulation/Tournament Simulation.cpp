08b94c5418732712b7c359b0b7e5c131d34b191e
Kian2002 <canadakurdistan@gmail.com>
2023-04-30 15:32:53 -0700

    Code Refactor

--- Tournament Simulation/Tournament Simulation.cpp
+++ Tournament Simulation/Tournament Simulation.cpp
@@ -7,6 +7,7 @@ void welcome();
 void roundOf16();
 std::vector<Group> createGroup(int size);
 void addTeams(const std::vector<Group>& group, const std::vector<Team>& teamsToAdd);
+std::vector<Team> startKnockoutMatch(const std::vector<Group>& groups, const std::string& title);
 
 
 int main()
@@ -20,19 +21,15 @@ int main()
 		seededTeams.push_back(teams[i]);
 	}
 
+	// Shrink the vector prevent teams from being added twice
 	teams.erase(teams.begin(), teams.begin() + 8);
 	teams.shrink_to_fit();
 
 	// create group stage groups
-	std::vector<Group> groups;
-	int groupIndex = 0;
+	std::vector<Group> groups = createGroup(8);
 	for (int i = 0; i < 8; i++)
 	{
-		Group group;
-		group.name = i + 1;
-		group.teams.push_back(seededTeams[groupIndex]);
-		groups.push_back(group);
-		groupIndex++;
+		groups[i].teams.push_back(seededTeams[i]);
 	}
 
 	// shuffle the teams so that the groups are more random
@@ -42,7 +39,7 @@ int main()
 	std::uniform_int_distribution<int> dist(-maxRandomOffset, maxRandomOffset);
 	std::shuffle(teams.begin(), teams.end(), rng);
 
-	// add the remaining teams to the groups
+	// add the remaining teams to the groups (three teams per group)
 	for (int i = 0; i < static_cast<int>(groups.size()); i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -52,7 +49,7 @@ int main()
 	}
 
 	// play the group stage
-	// the teams that advance to the knockout stage are stored in a vector
+	// the teams that advance to the round of 16 are stored in a vector
 	std::vector<Team> teamsThatAdvanceGroupStage;
 	for (int i = 0; i < static_cast<int>(groups.size()); i++)
 	{
@@ -61,12 +58,10 @@ int main()
 		teamsThatAdvanceGroupStage.push_back(returnedTeams[1]);
 	}
 
-	// create the knockout groups using the teams that advanced from the group stage
+	// create the round of 16 groups using the teams that advanced from the group stage
 	std::vector<Group> knockoutGroups = createGroup(static_cast<int>(teamsThatAdvanceGroupStage.size() / 2));
 
-	// add the teams that advance to the knockout stage to the knockout groups
-	// the teams are added in the following order by index:
-	// (0,3) (4,7) (8,11) (12,15) (1,2) (5,6) (9,10) (13,14)
+	// the teams are added in the following order by index: (0,3) (4,7) (8,11) (12,15) (1,2) (5,6) (9,10) (13,14)
 	int team_index = 0;
 	for (int i = 0; i < static_cast<int>(knockoutGroups.size()) / 2; i++)
 	{
@@ -79,36 +74,21 @@ int main()
 
 	// Commence Round of 16
 	roundOf16(); // print the round of 16 title
-	std::vector<Team> teamsThatAdvanceRoundOf16;
-	for (Group group : knockoutGroups)
-	{
-		Team team = group.playKnockoutMatch("Quarter Finals");
-		teamsThatAdvanceRoundOf16.push_back(team);
-	}
+	std::vector<Team> teamsThatAdvanceRoundOf16 = startKnockoutMatch(knockoutGroups, "Quarter Finals");
 
 	// Quarter Finals Groups
 	std::vector<Group> quarterFinalsGroups = createGroup(static_cast<int>(teamsThatAdvanceRoundOf16.size() / 2));
 	addTeams(quarterFinalsGroups, teamsThatAdvanceRoundOf16);
 
 	//Commence Quarter Finals
-	std::vector<Team> teamsThatAdvanceQuarterFinals;
-	for (Group group : quarterFinalsGroups)
-	{
-		Team team = group.playKnockoutMatch("Semi Finals");
-		teamsThatAdvanceQuarterFinals.push_back(team);
-	}
+	std::vector<Team> teamsThatAdvanceQuarterFinals = startKnockoutMatch(quarterFinalsGroups, "Semi Finals");
 
 	// Semi Finals Groups
 	std::vector<Group> semiFinalsGroups = createGroup(static_cast<int>(teamsThatAdvanceQuarterFinals.size() / 2));
 	addTeams(semiFinalsGroups, teamsThatAdvanceQuarterFinals);
 
 	// Commence Semi Finals
-	std::vector<Team> teamsThatAdvanceSemiFinals;
-	for (int i = 0; i < 2; i++)
-	{
-		Team team = semiFinalsGroups[i].playKnockoutMatch("Final");
-		teamsThatAdvanceSemiFinals.push_back(team);
-	}
+	std::vector<Team> teamsThatAdvanceSemiFinals = startKnockoutMatch(semiFinalsGroups, "Finals");
 
 	// Third Place Match
 	Group thirdPlaceGroup;
@@ -145,6 +125,17 @@ void addTeams(const std::vector<Group>& group, const std::vector<Team>& teamsToA
 		group[i].teams.push_back(teamsToAdd[i * 2]);
 		group[i].teams.push_back(teamsToAdd[i * 2 + 1]);
 	}
+}
+
+std::vector<Team> startKnockoutMatch(const std::vector<Group>& groups, const std::string& title)
+{
+	std::vector<Team> teamsThatAdvance;
+	for (Group group : groups)
+	{
+		Team team = group.playKnockoutMatch(title);
+		teamsThatAdvance.push_back(team);
+	}
+	return teamsThatAdvance;
 }
 
 void welcome()
